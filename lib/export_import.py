12e0381812c9f13b64a1c18d4ac848fbc266955a
Steven Schiff <steven.schiff@thomsonreuters.com>
2017-08-10 11:40:52 -0400

    Cleaned up a bunch

--- lib/export_import.py
+++ lib/export_import.py
@@ -6,14 +6,16 @@ from io import BytesIO
 from jira.client import JIRA
 from jira.exceptions import JIRAError
 
+
 def export_import_issues(source_jira, conf, query):
     target_jira = JIRA({'server': conf.JIRA['server']},
-                basic_auth=(conf.JIRA['user'], conf.JIRA['password']))
+                       basic_auth=(conf.JIRA['user'], conf.JIRA['password']))
     issues = source_jira.search_issues(query, maxResults=False)
     result = []
     print('About to export/import', len(issues), 'issues')
     _make_new_issues(source_jira, target_jira, issues, conf, result, None)
     return result
+
 
 def _make_new_issues(source_jira, target_jira, issues, conf, result, parent):
     for issue in issues:
@@ -22,9 +24,11 @@ def _make_new_issues(source_jira, target_jira, issues, conf, result, parent):
         # re-fetch to include comments and attachments
         issue = source_jira.issue(issue.key, expand='comments,attachments')
         fields = _get_new_issue_fields(issue.fields, conf)
+        print('conf', conf)
+        print('issue.fields', issue.fields)
+        print('fields', fields)
         if parent:
             fields['parent'] = {'key': parent.key}
-
         new_issue = target_jira.create_issue(fields=fields)
         if not parent:
             print('to', new_issue.key, '...', end=' ')
@@ -39,25 +43,26 @@ def _make_new_issues(source_jira, target_jira, issues, conf, result, parent):
                 _add_attachments(new_issue, target_jira, issue.fields.attachment)
             except JIRAError as e:
                 print('ERROR: attachment import failed with status',
-                        e.status_code, '...', end=' ')
+                      e.status_code, '...', end=' ')
                 target_jira.add_comment(new_issue, '*Failed to import attachments*')
         if issue.fields.subtasks:
             subtasks = [source_jira.issue(subtask.key) for subtask in
-                    issue.fields.subtasks]
+                        issue.fields.subtasks]
             print('with', len(subtasks), 'subtasks ...', end=' ')
             _make_new_issues(source_jira, target_jira, subtasks, conf, result, new_issue)
 
         comment = 'Imported from *[{1}|{0}/browse/{1}]*'.format(
-                source_jira._options['server'], issue.key)
+            source_jira._options['server'], issue.key)
         target_jira.add_comment(new_issue, comment)
         if conf.ADD_COMMENT_TO_OLD_ISSUE:
             comment = 'Exported to *[{1}|{0}/browse/{1}]*'.format(
-                    target_jira._options['server'], new_issue.key)
+                target_jira._options['server'], new_issue.key)
             source_jira.add_comment(issue, comment)
 
         result.append(new_issue.key)
         if not parent:
             print('done')
+
 
 def _get_new_issue_fields(fields, conf):
     result = {}
@@ -66,17 +71,30 @@ def _get_new_issue_fields(fields, conf):
         value = getattr(fields, name)
         if value is not None:
             result[name] = value
-    for name in ('priority', 'issuetype', 'assignee', 'reporter'):
-        value = getattr(fields, name)
-        if value:
-            value = getattr(value, 'name')
+    for name in ('priority','issuetype','reporter','assignee'):
+        if hasattr(fields, name):
+            field = getattr(fields, name)
+            value = getattr(field,'name')
             name_map = getattr(conf, name.upper() + '_MAP')
-            result[name] = {'name': name_map[value]}
+            try:
+                result[name] = {'name': name_map[value]}
+            except KeyError:
+                if name=='reporter':
+                    result[name] = {'name': conf.JIRA['user']}
+            print(result[name])
+    components=[]
+    for component in fields.components:
+        components.append({'name':getattr(component, 'name')})
+    result['components']=components
+
     if conf.CUSTOM_FIELD:
         result[conf.CUSTOM_FIELD[0]] = conf.CUSTOM_FIELD[1]
+    print(result.items())
     return result
 
+
 _g_epic_map = {}
+
 
 def _set_epic_link(new_issue, old_issue, conf, source_jira, target_jira):
     source_epic_key = getattr(old_issue.fields, conf.SOURCE_EPIC_LINK_FIELD_ID)
@@ -87,12 +105,13 @@ def _set_epic_link(new_issue, old_issue, conf, source_jira, target_jira):
         source_epic = source_jira.issue(source_epic_key)
         epic_fields = _get_new_issue_fields(source_epic.fields, conf)
         epic_fields[conf.TARGET_EPIC_NAME_FIELD_ID] = getattr(
-                source_epic.fields, conf.SOURCE_EPIC_NAME_FIELD_ID)
+            source_epic.fields, conf.SOURCE_EPIC_NAME_FIELD_ID)
         target_epic = target_jira.create_issue(fields=epic_fields)
         _g_epic_map[source_epic_key] = target_epic
     target_epic = _g_epic_map[source_epic_key]
     target_jira.add_issues_to_epic(target_epic.key, [new_issue.key])
     print('linked to epic', target_epic.key, '...', end=' ')
+
 
 def _set_status(new_issue, old_issue, conf, target_jira):
     status_name = old_issue.fields.status.name
@@ -103,14 +122,16 @@ def _set_status(new_issue, old_issue, conf, target_jira):
         if isinstance(transition_name, conf.WithResolution):
             resolution = conf.RESOLUTION_MAP[old_issue.fields.resolution.name]
             target_jira.transition_issue(new_issue, transition_name.transition_name,
-                    fields={'resolution': {'name': resolution}})
+                                         fields={'resolution': {'name': resolution}})
         else:
             target_jira.transition_issue(new_issue, transition_name)
+
 
 def _add_comments(issue, jira, comments):
     for comment in comments:
         jira.add_comment(issue, u"*Comment by {0}*:\n{1}"
-                .format(comment.author.displayName, comment.body))
+                         .format(comment.author.displayName, comment.body))
+
 
 def _add_attachments(issue, jira, attachments):
     for attachment in attachments:
@@ -118,12 +139,13 @@ def _add_attachments(issue, jira, attachments):
             for chunk in attachment.iter_content():
                 buf.write(chunk)
             jira.add_attachment(issue,
-                    filename=_normalize_filename(attachment.filename),
-                    attachment=buf)
+                                filename=_normalize_filename(attachment.filename),
+                                attachment=buf)
+
 
 def _normalize_filename(value):
     return unicodedata.normalize('NFKD', value).encode('ascii',
-            'ignore').decode('ascii')
+                                                       'ignore').decode('ascii')
 
 # -------------------------------------
 # TODO:
